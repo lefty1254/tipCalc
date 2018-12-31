fd73e0ea06b8eb5d7a933b8f50a146c548ac5731
Eleftherios Troullouris <Lefteris@dimitriss-air.fios-router.home>
2018-12-31 06:43:14 -0500

    Initial Commit

---
+++ tipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalc
+//
+//  Created by Eleftherios Troullouris on 12/31/18.
+//  Copyright © 2018 Eleftherios Troullouris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
